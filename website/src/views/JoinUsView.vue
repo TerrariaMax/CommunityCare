c927e7372f797319d60407f6359bc21eef65fa87
Moana <flowingisland666@gmail.com>
2024-10-19 23:32:28 +1100

    test

--- website/src/views/JoinUsView.vue
+++ website/src/views/JoinUsView.vue
@@ -389,7 +389,7 @@ export default {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords
           //API link to obtain the current weather based on the current location browser identified
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
           //await means wait for the fetchWeatherData method to complete before proceeding
           await this.fetchWeatherData(url)
         })
@@ -406,7 +406,7 @@ export default {
     },
     async searchByCity() {
       if (!this.city) return
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
       await this.fetchWeatherData(url)
     }
   }
